d7d5b8f0502f4231eb3b91acc30db3b00278ccf4
Rohan kumar <rohan7690031677@gmail.com>
2023-11-23 17:09:26 +0530

    ball changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -116,7 +116,7 @@ import {
       company_name: "RightLabs",
       icon: rightlabs,
       iconBg: "#383E56",
-      date: "March 2020 - April 2021",
+      date: "August 2023 - September 2023",
       points: [
         "Developing and maintaining web applications using React.js and other related technologies.",
         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
